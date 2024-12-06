5ee8b3aef647a739dab036354b2d53d2f8f1cad5
Riju-88 <rijumistri4@gmail.com>
2024-12-06 23:20:08 +0530

    1. Added Payment model and migration for razorpay. 2. Modified RazorpayPaymentController's store method to include the same code as Shop Portal.

--- app/Http/Controllers/RazorpayPaymentController.php
+++ app/Http/Controllers/RazorpayPaymentController.php
@@ -27,27 +27,45 @@ class RazorpayPaymentController extends Controller
      *
      * @return response()
      */
-    public function store(Request $request): RedirectResponse
+    public function store(Request $request)
     {
         $input = $request->all();
-
-        $api = new Api(env('RAZORPAY_KEY'), env('RAZORPAY_SECRET'));
-
+         // Verify CSRF token
+         if (!Session::token() == $input['_token']) {
+            Session::put('error', 'CSRF token mismatch');
+            return redirect()->back();
+        }
+        $api = new Api(env('RAZORPAY_API_KEY'), env('RAZORPAY_API_SECRET'));
+        // dd($input);
         $payment = $api->payment->fetch($input['razorpay_payment_id']);
-
-        if (!empty($input['razorpay_payment_id'])) {
+        if (count($input) && !empty($input['razorpay_payment_id'])) {
             try {
-                $response = $api
-                    ->payment
-                    ->fetch($input['razorpay_payment_id'])
-                    ->capture(['amount' => $payment['amount']]);
+                $response = $api->payment->fetch($input['razorpay_payment_id'])->capture(array('amount' => $payment['amount']));
+                $payment = Payment::create([
+                    'r_payment_id' => $response['id'],
+                    'method' => $response['method'],
+                    'currency' => $response['currency'],
+                    'user_email' => $response['email'],
+                    'amount' => $response['amount'] / 100,
+                    'json_response' => json_encode((array) $response)
+                ]);
+
+                // if ($response['status'] == 'captured') {
+                //     DB::transaction(function () {
+                       
+                //     });
+                // }
             } catch (Exception $e) {
+                \Log::error('Razorpay API Error: ' . $e->getMessage());
+                Session::put('error', 'An error occurred while processing the payment. Please try again later.');
+                // return redirect(route('home'));
                 return redirect()
                     ->back()
-                    ->with('error', $e->getMessage());
+                    ->with('error', 'An error occurred while processing the payment. Please try again later.');
             }
         }
-
+        // Session::put('success', ('Payment Successful'));
+        // return redirect(route('home'));
         return redirect()
             ->back()
             ->with('success', 'Payment successful');
